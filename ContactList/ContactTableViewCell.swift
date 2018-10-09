ac10f15cf6ec1edfbb90492573cc46f14fee994a
Minami Munakata <june28munazo@gmail.com>
2018-10-09 16:03:36 -0700

    set cell leyout

---
+++ ContactList/ContactTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContactTableViewCell.swift
+//  ContactList
+//
+//  Created by minami on 2018-10-09.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ContactList/ViewController.swift
+++ ContactList/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    static var contacts = [["Kenta","Japan"], ["Minami","Japan"], ["Marcelo","Brasil"], ["Giada","Italy"],
+                            ["Nagisa","Japan"], ["Bianca","Brasil"], ["Yukako","Japan"], ["Rei","Japan"],
+                            ["Alex","South Korea"], ["Javier","Maexico"], ["Juan","Mexico"], ["Natsumi","Japan"],
+                            ["Suelen","Brasil"], ["Marla","Brasil"], ["Emre E","Turkey"], ["Gustavo","Brasil"],
+                            ["Chin","Thailand"], ["Charles","China"], ["Emre A","Turkey"]]
 
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.rowHeight = 90
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ViewController.contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ContactTableViewCell
+        let name = ViewController.contacts[indexPath.row][0]
+        let country = ViewController.contacts[indexPath.row][1]
+        cell.nameLabel.text = name
+        cell.countryLabel.text = country
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
 }
 
